6b420c7039bf6134d5d44c01d139e08192036af8
Arnas Petkevicius <arnas.pet@gmail.com>
2018-02-03 16:56:30 +0200

    first commit

---
+++ AndroidProject/app/src/main/java/com/example/android/middleearth/QuizzClass.java
@@ -0,0 +1,18 @@
+package com.example.android.middleearth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ARN on 2/3/2018.
+ */
+
+public class QuizzClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quizz);
+    }
+
+}
