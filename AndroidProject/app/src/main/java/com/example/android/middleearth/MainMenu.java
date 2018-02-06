be5931f0b517b53d3891b0d385ae3c2f90dbd684
Arnas Petkevicius <arnas.pet@gmail.com>
2018-02-06 17:10:08 +0200

    final commit

--- AndroidProject/app/src/main/java/com/example/android/middleearth/MainMenu.java
+++ AndroidProject/app/src/main/java/com/example/android/middleearth/MainMenu.java
@@ -1,13 +1,44 @@
 package com.example.android.middleearth;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainMenu extends AppCompatActivity {
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
 
+
+public class MainMenu extends Activity {
+
+
+
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
     }
+
+
+
+
+            public void onClickSelect(View view) {
+
+                String name = ((EditText) findViewById(R.id.name_text_view)).getText().toString();
+
+                Toast toast = Toast.makeText(getApplicationContext(),"Hi "+ name + " let's dive in!", Toast.LENGTH_LONG);
+                toast.show();
+
+                Intent intent = new Intent(getApplicationContext(), QuizzClass.class);
+                startActivity(intent);
+            }
+
+
+
+
+
 }
--- AndroidProject/app/src/main/java/com/example/android/middleearth/QuizzClass.java
+++ AndroidProject/app/src/main/java/com/example/android/middleearth/QuizzClass.java
@@ -2,6 +2,11 @@ package com.example.android.middleearth;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 /**
  * Created by ARN on 2/3/2018.
@@ -9,10 +14,85 @@ import android.support.v7.app.AppCompatActivity;
 
 public class QuizzClass extends AppCompatActivity {
 
+    private int overallScore;
+    private final int totalScore = 10;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quizz);
     }
+
+    public void checkIfCorrect(View view) {
+        if (((RadioButton) findViewById(R.id.q1_radio_answer_1)).isChecked()) {
+            overallScore = overallScore + 1;
+
+        }
+        if (((RadioButton) findViewById(R.id.q2_radio_answer_3)).isChecked()) {
+            overallScore = overallScore + 1;
+        }
+        if (((RadioButton) findViewById(R.id.q3_radio_answer_4)).isChecked()) {
+            overallScore = overallScore + 1;
+        }
+        if (((RadioButton) findViewById(R.id.q4_radio_answer_4)).isChecked()) {
+            overallScore = overallScore + 1;
+        }
+        if (((RadioButton) findViewById(R.id.q6_radio_answer_2)).isChecked()) {
+            overallScore = overallScore + 1;
+        }
+        if (((RadioButton) findViewById(R.id.q7_radio_answer_3)).isChecked()) {
+            overallScore = overallScore + 1;
+        }
+        if (((RadioButton) findViewById(R.id.q8_radio_answer_2)).isChecked()) {
+            overallScore = overallScore + 1;
+        }
+        if (((RadioButton) findViewById(R.id.q9_radio_answer_1)).isChecked()) {
+            overallScore = overallScore + 1;
+        }
+        if (((RadioButton) findViewById(R.id.q10_radio_answer_4)).isChecked()) {
+            overallScore = overallScore + 1;
+        }
+        if (checkIfCorrectBox(view)) {
+            overallScore = overallScore + 1;
+        }
+        toastMessage(makeMessage());
+        overallScore =0;
+    }
+
+
+
+    private boolean checkIfCorrectBox(View view) {
+        CheckBox box1 = (CheckBox) findViewById(R.id.q5_check_answer_1);
+        CheckBox box2 = (CheckBox) findViewById(R.id.q5_check_answer_2);
+        CheckBox box3 = (CheckBox) findViewById(R.id.q5_check_answer_3);
+        CheckBox box4 = (CheckBox) findViewById(R.id.q5_check_answer_4);
+
+        if (box1.isChecked() && box2.isChecked() && box4.isChecked() && !box3.isChecked()) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void toastMessage(String message) {
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
+    private String makeMessage() {
+        String message;
+        if (overallScore == totalScore) {
+            message = getString(R.string.winner);
+        } else {
+            message =  overallScore+" out of "+ totalScore +"." + getString(R.string.looser);
+        }
+        return message;
+    }
+
 
 }
